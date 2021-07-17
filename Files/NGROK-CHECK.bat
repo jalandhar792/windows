aef3e43a719f21a49c5f6e11547aee4f7bef729f
jalandhar792 <61079999+jalandhar792@users.noreply.github.com>
2021-07-17 11:27:21 +0530

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -10,7 +10,7 @@ tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -
 echo User: Administrator
 echo Pass: @Jalandhar
 curl -O https://raw.githubusercontent.com/jalandhar792/windows/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop" https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/jalandhar792/windows/blob/main/Files/WinRAR.zip > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop" https://github.com/jalandhar792/windows/blob/main/Files/WinRAR.zip?raw=true > out.txt 2>&1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
 diskperf -Y >nul
 sc start audiosrv >nul
