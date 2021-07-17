10d80aac712d4ab7d88b77f25653df0b731eb015
jalandhar792 <61079999+jalandhar792@users.noreply.github.com>
2021-07-17 14:46:03 +0530

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -12,7 +12,7 @@ echo Pass: @Jalandhar
 curl -O https://raw.githubusercontent.com/jalandhar792/windows/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
 
 C:\Users\administrator\Desktop>svn export https://github.com/jalandhar792/windows/trunk/Files/WinRAR.zip
-PowerShell -Command "Expand-Archive WinRAR.zip -DestinationPath C:\Users\administrator\Desktop\WinRAR
+::PowerShell -Command "Expand-Archive WinRAR.zip -DestinationPath C:\Users\administrator\Desktop\WinRAR
 ::del WinRAR.zip
 
 ::curl -o "C:\Users\Public\Desktop/WinRAR/zipnew.dat" https://raw.githubusercontent.com/jalandhar792/windows/main/Files/WinRAR/zipnew.dat > out.txt 2>&1
